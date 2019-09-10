f19c1d8689baf3b9c160f87ec80217726a1fef9b
Avinash Salimath <asalimath@ml-c02xfez8jg5h.wsgc.com>
2019-09-10 11:11:59 +0530

    Code Refactoring

--- cypress/integration/Tests/sampleSpec.js
+++ cypress/integration/Tests/sampleSpec.js
@@ -1,14 +1,30 @@
-describe('This is first test',function(){
-    it('This is first test case',function(){
+/// <reference types="Cypress" />
+
+
+describe('This is first test', function () {
+    it('This is first test case', function () {
+
+        //Navigate to Amazon.in
         cy.visit('https://www.amazon.in')
 
+        /*
+        Locate Search box
+        Type "One Plus Mobile" text
+        */
         cy.get('#twotabsearchtextbox')
-          .type('One Plus Mobile')
+            .type('One Plus Mobile')
 
-          cy.get('.nav-search-submit > .nav-input').click()  
+        //Click on search button
+        cy.get('.nav-search-submit > .nav-input').click()
 
-          
+        //Assert that search results has item count equal to 20
+        cy.get('.s-result-list.s-search-results.sg-row > div').should('have.length.greaterThan',0)//.should('have.length',20)
+
+        //Among the search results click on element with text 'OnePlus 7 Pro (Nebula Blue, 12GB RAM, 256GB Storage)
+        cy.get('.s-result-list.s-search-results.sg-row > div').contains('OnePlus 7 Pro (Nebula Blue, 12GB RAM, 256GB Storage)').click()
+        
+
     })
 
-    
+
 })
