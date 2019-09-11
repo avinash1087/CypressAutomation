d8a36d3ba8a515e0eba8b243e9c0858a27a36f73
Avinash Salimath <asalimath@ml-c02xfez8jg5h.wsgc.com>
2019-09-11 15:25:33 +0530

    Added Test Case for DropDown Validation

--- cypress/integration/Tests/TextBox_Button_EachLoop.js
+++ cypress/integration/Tests/TextBox_Button_EachLoop.js
@@ -7,6 +7,7 @@ describe('Automating Amazon Tests', function () {
         //Navigate to Amazon.in
         cy.visit('https://www.amazon.in')
 
+
         /*
         Locate Search box
         Type "One Plus Mobile" text
@@ -18,17 +19,18 @@ describe('Automating Amazon Tests', function () {
         cy.get('.nav-search-submit > .nav-input').click()
 
         //Assert that search results has item count equal to 20
-        //cy.get('.s-result-list.s-search-results.sg-row > div').should('have.length.greaterThan',0)//.should('have.length',20)
+        cy.get('.s-result-list.s-search-results.sg-row > div').should('have.length.greaterThan',0)//.should('have.length',20)
 
         //Among the search results click on element with text 'OnePlus 7 Pro (Nebula Blue, 12GB RAM, 256GB Storage)
         //cy.get('.s-result-list.s-search-results.sg-row > div').contains('OnePlus 7 Pro (Nebula Blue, 12GB RAM, 256GB Storage)').click()
-        
-        cy.get('span.a-size-medium.a-color-base.a-text-normal').each(function($e1,index,$list) {
-            
-            const productText = $e1.text()
 
-            if(productText.includes('Almond')){
+        cy.get('span.a-size-medium.a-color-base.a-text-normal').each(function ($e1, index, $list) {
+
+            const productText = $e1.text()
+            cy.log('Item Text: ' + productText);
+            if (productText.includes('Almond')) {
                 $e1.click();
+                return;
             }
         })
 
